02e19356cd9813b7d28ed6d4fdc9885c7a894bea
H. Okano <h.okano@gmail.com>
2017-05-23 20:10:31 +0900

    autosave

--- edit.js
+++ edit.js
@@ -1,28 +1,31 @@
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
 } else {
-  alert('The File APIs are not fully supported in this browser.');
+    alert('The File APIs are not fully supported in this browser.');
 }
 
 var storage = localStorage;
 
 document.title = "SimpleEdit(js)";
 
-//Save text to local storage
+// save text to local storage
 function saveContent(){
     var textContent = document.getElementById("textarea");
     var key = "saveeditor";
     var value = textContent.innerText;
     storage.setItem(key, value);
+    document.getElementById('textarea').focus();
 }
 
-//load text from local storage
+// load text from local storage
 function loadContent(){
     var loaded = storage.getItem("saveeditor");
     document.getElementById("textarea").innerText = loaded;
+    document.getElementById('textarea').focus();
 }
 
-//clear textarea
+// clear textarea
 function cleartextarea(){
-        document.getElementById("textarea").innerText = "";
+    document.getElementById("textarea").innerText = "";
+    document.getElementById('textarea').focus();
 }
